b7cee0eae1de2b8d1f002289bf27158ea9303641
good_andre <good-andre@yandex.ru>
2020-12-22 19:08:05 +0300

    second

--- scripts/index.js
+++ scripts/index.js
@@ -10,7 +10,7 @@ const deactivationPlayer = () => {
   temp.style.display = 'none';
   playerBtn.forEach(item => item.classList.remove('active'));
   playerBlock.forEach(item => item.classList.remove('active'));
-};
+}
 
 playerBtn.forEach((btn, i) => btn.addEventListener('click', () => {
     deactivationPlayer();
--- scripts/musicPlayer.js
+++ scripts/musicPlayer.js
@@ -1,4 +1,3 @@
 export const musicPlayerInit = () => {
   console.log('Music init');
 }
-
